906dd29dea18d80d3bae2754b15e663129dd5a84
unknown <bardhoshieugen2@gmail.com>
2024-03-04 20:30:54 +0000

    about section created

--- Eugens-React-Portfolio/src/components/Hero/Hero.jsx
+++ Eugens-React-Portfolio/src/components/Hero/Hero.jsx
@@ -4,18 +4,25 @@ import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
   return (
-    <section className={styles.heroSection}>
-      <div>
-        <h1>Hi, I'm Eugen</h1>
-     
-      <p>
-        I'm a front-end developer with 2 years of experience using React and Node.js.
-        Reach out if you would like to work together.
+    <section className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Hi, I'm Eugen</h1>
+      <p className={styles.description}>
+        I'm a front end developer with 2 years of experience using React and
+        NodeJS. Reach out if you'd like to learn more!
       </p>
-      <a href="mailto:bardhoshieugen2@gmail.com">Contact me</a>
-      </div>
-    <img src={getImageUrl("hero/eugens2 photo.jpg.png")} alt="hero" />
-    </section>
+      <a href="mailto:bardhoshieugen2@gmail.com" className={styles.contactBtn}>
+        Contact Me
+      </a>
+    </div>
+    <img
+      src={getImageUrl("hero/eugens2 photo.jpg.png")}
+      alt="Eugens Photo"
+      className={styles.heroImg}
+    />
+    <div className={styles.topBlur} />
+    <div className={styles.bottomBlur} />
+  </section>
 
 
   );
